4bad85e38525571b17697ef8cc0ae0b859a95b20
anestercommprod <108794312+anestercommprod@users.noreply.github.com>
2022-09-09 02:09:22 +0300

    C# Source code
    Visual Studio project baseline sources.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Diagnostics;
+using System.Threading;
+
+namespace disableWinUpdate
+{
+    class Program
+    {
+        static void Main()
+        {
+            Process.Start("cmd.exe", "/K cls && sc config wuauserv start=disabled && sc stop wuauserv && sc stop WaaSMedicSvc && echo 'updates paused, at least, for now...' && pause && exit");
+            Thread.Sleep(1);
+            System.Environment.Exit(1);
+        }
+    }
+}
